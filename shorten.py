bc0d7d98850515b2207a5e09ff66dfec442ff14d
Anuj Saxena <anuj.saxena.dev@gmail.com>
2019-12-29 11:16:12 +0530

    Setting Parameters and Sending POST request from the rel.ink API

--- shorten.py
+++ shorten.py
@@ -4,3 +4,10 @@ import requests
 print("URL Shortener")
 print("#############")
 link = input("Enter URL to shorten : ")
+
+
+# Setting Parameters and Sending POST request from the rel.ink API
+url = 'https://rel.ink/api/links/'
+data = {"url": link}
+response = requests.post(url, data = data)
+
