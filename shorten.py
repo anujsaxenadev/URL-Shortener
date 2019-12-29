de892913581b111f98889746ddd06f13adcf8395
Anuj Saxena <anuj.saxena.dev@gmail.com>
2019-12-29 11:16:54 +0530

    Validating Response and Printing the Shorten URL

--- shorten.py
+++ shorten.py
@@ -11,3 +11,9 @@ url = 'https://rel.ink/api/links/'
 data = {"url": link}
 response = requests.post(url, data = data)
 
+# Validating Response and Printing the Shorten URL
+if response:
+	id = response.json()["hashid"]
+	print("Shorten URL : https://rel.ink/" + id)
+else:
+	print("Please Enter a valid URL")
